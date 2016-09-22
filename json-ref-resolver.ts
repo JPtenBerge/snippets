228900b850d7844242cfa6a6eca203db1ab735d0
JP ten Berge <jpeetjuh@hotmail.com>
2016-09-22 16:30:03 +0200

    De JSON-refresolver van AngularJS omgezet naar Angular 2. Het is geen interceptor meer, die kent Angular 2 niet, dus gewoon een injectable die je aan moet spreken. Voordeel bij deze interceptor is dat Angular 2 geen digest cycle meer kent, angular.copy() niet meer bestaat en het filter filter ook niet, dus er hoeft geen rekening te worden gehouden met of een referentie in een bepaalde hierarchie mag worden geresolved of niet. Bij deze implementatie wordt alles gewoon plat geresolved. Plus het is TypeScript, dus we kunnen modernere code gebruiken.

---
+++ json-ref-resolver.ts
@@ -0,0 +1,81 @@
+import { Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class JsonRefResolver {
+	transform(resp: Response) {
+		
+		if (!resp.headers.get('Content-Type') || resp.headers.get('Content-Type').startsWith('application/json') === false) {
+			return resp;
+		}
+
+		let idObjects: any[] = [];
+		let objects = resp.json();
+		findIdObjects(objects);
+		resolve(objects);
+		cleanup();
+
+		return objects;
+
+		function findIdObjects(data: any) {
+			// Is data an array? The top-level data is an array. Go through the array and parse every object in the array.
+			if (Array.isArray(data)) {
+				data.forEach((item: any) => findIdObjects(item));
+				return;
+			}
+
+			if (data && typeof data === 'object') {
+				if ('$id' in data) {
+					idObjects.push(data);
+				}
+
+				// Look for nested objects to go through recursively
+				for (var key in data) {
+					// For an array or object, recursively call this function again, until the point that we can parse primitive values
+					// We're only checking for isObject() here, since isObject() gives true for an array as well.
+					if (typeof data[key] === 'object') {
+						findIdObjects(data[key]);
+					}
+				}
+			}
+		}
+
+		function resolve(data: any) {
+			if (Array.isArray(data)) {
+				for (let i = 0; i < data.length; i++) {
+					// If there's a $ref, then resolve it
+					if (data[i] && data[i].$ref !== undefined) {
+						data[i] = idObjects.find(idObject => idObject.$id === data[i].$ref);
+					}
+					else {
+						resolve(data[i]);
+					}
+				}
+				return;
+			}
+
+			if (data && typeof data === 'object') {
+				// Go through every property looking for nested objects/arrays
+				for (var prop in data) {
+					if (data[prop] && data[prop].$ref !== undefined) {
+						data[prop] = idObjects.find(idObject => idObject.$id === data[prop].$ref);
+					}
+					else if (typeof data[prop] === 'object') {
+						resolve(data[prop]);
+					}
+				}
+				return;
+			}
+		}
+
+		function getIdObject(id: any) {
+			return;
+		}
+
+		function cleanup() {
+			idObjects.forEach(idObject => {
+				delete idObject.$id;
+			});
+		}
+	}
+}
