27c28a7c5896adee344bacd4386bf63732697c72
JP <3285470+JPtenBerge@users.noreply.github.com>
2019-05-28 12:37:54 +0200

    Changed filename

--- json-ref-resolver.ts
+++ json-ref-resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class JsonResolver {
+export class JsonRefResolver {
 	transform(objects: Object | Object[]) {
 		let idObjects = [];
 		findIdObjects(objects);
